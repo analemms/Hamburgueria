af2e791a663a73c7f856215de55f1fdeaf19248f
analemms <analemos457@gmail.com>
2022-12-07 21:22:15 -0300

    Commit test

--- src/main/java/br/com/hamburgueria/HamburgueriaApp.java
+++ src/main/java/br/com/hamburgueria/HamburgueriaApp.java
@@ -1,5 +1,6 @@
 package br.com.hamburgueria;
 
+import br.com.hamburgueria.pojo.ItemIncluso;
 import br.com.hamburgueria.pojo.Produto;
 
 import java.util.ArrayList;
@@ -10,18 +11,40 @@ public class HamburgueriaApp { //classe principal
         Produto meuProduto = new Produto(); //criando um novo produto
 
         meuProduto.setNome("Combo 1");
-        meuProduto.setValor(30);
+        meuProduto.setValor(49.99);
         meuProduto.setTamanho("Serve 2 pessoas");
 
-        List<String> itensInclusos = new ArrayList<>(); //criação de uma variável itens inclusos sendo uma lista de strings
-        itensInclusos.add("2 Hamburguers artesanais"); //adicionando itens na lista
-        itensInclusos.add("2 Porções de batata frita");
-        itensInclusos.add("2 Refrigerantes");
-        meuProduto.setItensInclusos (itensInclusos);
 
+        List<ItemIncluso> itensInclusos = new ArrayList<>(); //criação de uma variável itens inclusos sendo uma lista de itens
+
+        ItemIncluso primeiroItemIncluso = new ItemIncluso("Hamburguers artesanais", 2);//objeto representados por classes
+        itensInclusos.add(primeiroItemIncluso); //adicionando itens na lista
+
+        ItemIncluso segundoItemIncluso = new ItemIncluso("Porções de batata frita", 2);
+        itensInclusos.add(segundoItemIncluso);
+
+        ItemIncluso terceiroItemIncluso = new ItemIncluso("Refrigerantes", 2);
+        itensInclusos.add(terceiroItemIncluso);
+
+        meuProduto.setItensInclusos(itensInclusos);
+
+        System.out.println(meuProduto.getNome());
         System.out.println(meuProduto.getItensInclusos().size()); //pegando meus itens e mostrando o tamanho da lista
-
         System.out.println(meuProduto.getValor());
+        System.out.println(meuProduto.getMarca()); //já foi definido, a marca vem antes
+
+        System.out.println(meuProduto.getItensInclusos().get(0).getNome());
+        System.out.println(meuProduto.getItensInclusos().get(0).getQuantidade());
+
+        System.out.println("Começando a apresentar os itens");
+
+        for (ItemIncluso itemAtual : meuProduto.getItensInclusos()) {//Laços - itens inclusos é a lista - mostrar o nome e qntd de cada um dos itens
+            System.out.println(itemAtual.getNome());
+            System.out.println(itemAtual.getQuantidade());
+        }
+
+        System.out.println("Acabaram-se os itens");
+
     }
 
 }
--- src/main/java/br/com/hamburgueria/pojo/Produto.java
+++ src/main/java/br/com/hamburgueria/pojo/Produto.java
@@ -2,15 +2,21 @@ package br.com.hamburgueria.pojo;
 
 import java.util.List;
 
-public class Produto { //classe produto e seus atributos
-    private String nome;//encapsulamento dos atributos - classe restrita
+public class Produto { //classe produto (modificador, class e nome da class) e seus atributos(modificador, tipo e nome do atributo)
+    private String nome;//encapsulamento dos atributos - classe restrita - tipo da variável e nome
     private double valor;
     private String tamanho;
-    private List<String> itensInclusos; //Classe e Estrutura de dado - lista
+    private String marca;
+    private List<ItemIncluso> itensInclusos; //Classe e Estrutura de dado - lista
+
+    public Produto () { //estrutura de um construtor (modificador e nome da classe)
+        this.marca = "Vegan";
+
+    }
 
     public double getValor() { //parâmetro de busca de valor
         return this.valor;
-    }
+    } //método: modificador, tipo do retorno, nome do método
 
     public void setValor(double novoValor) { //parâmetro de atribuição de valor
         if (novoValor > 0) { //Estrutura de decisão
@@ -36,11 +42,19 @@ public class Produto { //classe produto e seus atributos
         this.tamanho = novoTamanho;
     }
 
-    public List<String> getItensInclusos() {
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String novaMarca) {
+        this.marca = novaMarca;
+    }
+
+    public List<ItemIncluso> getItensInclusos() {
         return itensInclusos;
     }
 
-    public void setItensInclusos(List<String> novosItensInclusos) {
+    public void setItensInclusos(List<ItemIncluso> novosItensInclusos) {
         this.itensInclusos = novosItensInclusos;
     }
 }
